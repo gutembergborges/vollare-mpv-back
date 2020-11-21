6c9fb7143e4f4f359dfbb20693328f9747d43711
gutembergborges <gutem.borges@gmail.com>
2020-11-21 15:55:42 -0300

    stablished communication with mongodb cloud

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const restify = require ('restify')
+const mongoose = require('mongoose')
 
-const server = restify.createServer({
-    name: 'vollare-mpv-back',
-    version: '0.0.1'
-})
-
-const users = [
-    {id: 'b739fba7-cdea-42f9-8d22-7b7a80455d80', name: 'Xiang Ling'},
-    {id: '8129764b-eab0-4235-bb2e-b36b0f2ec70e', name: 'Ast Mona'}
-]
-
-server.get('/users', (req, resp, next)=>{
-    resp.json(users)
-})
-
-server.get('/users/:id', (req, resp, next)=>{
-    const filtered = users.filter(user => user.id === req.params.id)
-    if(filtered.length){
-        resp.json(filtered[0])
-    } else {
-        resp.status(404)
-        resp.json({message: 'not found'})
-    }
-    return next()
-})
-
-server.listen(8080, ()=>{
-    console.log('API listening on 8080')
-})
+// criar um .env para guardar essas variáveis de ambiente e nao expô-las
+// mongodb+srv://<username></username>:<password>@cluster0.y1xkp.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose.connect('mongodb+srv://cloudmongo:cloudmongo@cluster0.y1xkp.mongodb.net/vollare-mpv-backend')
+        .then(_=>{
+            const server = restify.createServer({
+                name: 'vollare-mpv-back',
+                version: '0.0.1'
+            })
+            
+            const users = [
+                {id: 'b739fba7-cdea-42f9-8d22-7b7a80455d80', name: 'Xiang Ling'},
+                {id: '8129764b-eab0-4235-bb2e-b36b0f2ec70e', name: 'Ast Mona'}
+            ]
+            
+            server.get('/users', (req, resp, next)=>{
+                resp.json(users)
+            })
+            
+            server.get('/users/:id', (req, resp, next)=>{
+                const filtered = users.filter(user => user.id === req.params.id)
+                if(filtered.length){
+                    resp.json(filtered[0])
+                } else {
+                    resp.status(404)
+                    resp.json({message: 'not found'})
+                }
+                return next()
+            })
+            
+            server.listen(8080, ()=>{
+                console.log('API listening on 8080')
+            })
+        }).catch(console.error)
