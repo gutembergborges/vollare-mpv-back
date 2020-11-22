03e7bd86a8587d53f47e784a1047146573cc7417
gutembergborges <gutem.borges@gmail.com>
2020-11-22 13:21:04 -0300

    fixed `findOneAndUpdate()` Deprecation Warning

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+// Module dependencies
 const config = require('./config')
 const restify = require('restify')
 const mongoose = require('mongoose')
 
-
+// Initialize server
 const server = restify.createServer({
     name: config.name,
     version: config.version
 })
 
+// Middleware
 // We need a plugin to do Parse of Request body
 server.use(restify.plugins.bodyParser())
+// Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
+// by default, you need to set it to false.
+mongoose.set('useFindAndModify', false);
 
+// Start server, connect to DB and require routes
 // Select server port to listen
 server.listen(config.port, () => {
     // establish connection to mongodb
