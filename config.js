19453cd0eeec39dd4afc2f498d5c459637b46452
gutembergborges <gutem.borges@gmail.com>
2020-11-22 13:16:19 -0300

    reorganized database and enviroment variables

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'vollare-mpv-back',
+    version: '1.0.0',
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 8080,
+    base_url: process.env.BASE_URL || 'http://localhost:8080',
+    db: {
+        /*  using a mongodb cloud database: https://cloud.mongodb.com/v2/5fb9335ef712175c32e5b6ed#clusters
+            mongodb+srv://<username></username>:<password>@cluster0.y1xkp.mongodb.net/<dbname>?retryWrites=true&w=majority
+        */
+        uri: process.env.MONGODB_URI || 'mongodb+srv://cloudmongo:cloudmongo@cluster0.y1xkp.mongodb.net/vollare-mpv-backend',
+    },
+}
--- index.js
+++ index.js
@@ -1,24 +1,21 @@
+const config = require('./config')
 const restify = require('restify')
 const mongoose = require('mongoose')
 
-/* * criar um .env para guardar essas variáveis de ambiente e nao expô-las
-    using a mongodb cloud database: https://cloud.mongodb.com/v2/5fb9335ef712175c32e5b6ed#clusters
-    mongodb+srv://<username></username>:<password>@cluster0.y1xkp.mongodb.net/<dbname>?retryWrites=true&w=majority
-*/
 
 const server = restify.createServer({
-    name: 'vollare-mpv-back',
-    version: '0.0.1'
+    name: config.name,
+    version: config.version
 })
 
 // We need a plugin to do Parse of Request body
 server.use(restify.plugins.bodyParser())
 
 // Select server port to listen
-server.listen(8080, () => {
+server.listen(config.port, () => {
     // establish connection to mongodb
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb+srv://cloudmongo:cloudmongo@cluster0.y1xkp.mongodb.net/vollare-mpv-backend', { useUnifiedTopology: true, useNewUrlParser: true });
+    mongoose.connect(config.db.uri, { useUnifiedTopology: true, useNewUrlParser: true });
     const db = mongoose.connection;
     db.on('error', (err) => {
         console.error(err);
