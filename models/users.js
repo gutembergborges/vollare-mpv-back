2d110662bc6f4001b41763adf7c40838c7ef3e48
gutembergborges <gutem.borges@gmail.com>
2020-11-24 11:10:27 -0300

    improved users model validators

--- models/users.js
+++ models/users.js
@@ -4,21 +4,27 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        maxlength: 80,
+        minlength: 3
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     },
     password: {
         type: String,
         required: true,
+        maxlength: 30,
+        minlength: 4,
         select: false       // by default don't show password
     },
     role: {
         type: String,
-        required: true
+        required: true,
+        enum: ['Administrador', 'Atendimento', 'Certidão', 'Registro']
     }
 })
 
