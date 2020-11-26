a4f6c36c48b2bbb64c108faea397a5ed47e6e057
gutembergborges <gutem.borges@gmail.com>
2020-11-25 21:57:41 -0300

    created patch and wrong get to user tests

--- tests/users.test.js
+++ tests/users.test.js
@@ -28,11 +28,46 @@ test('post /users', () => {
         })
         .then(response => {
             expect(response.status).toBe(200)   // awaits response status is 200
-            // expect(response.body).toBeInstanceOf(Array)
             expect(response.body._id).toBeDefined()
             expect(response.body.name).toBe('admin')
             expect(response.body.email).toBe('admin@vollare.com')
             expect(response.body.role).toBe('Administrador')
             expect(response.body.password).toBeUndefined()   // because don't return password
         }).catch(fail)  // global function to transform error in fail test
+})
+
+// testing GET id with wrong id, expects error
+test('get /users/aaaa - not found', () => {
+    return request(config.base_url)     // return to use a request like a promise
+        .get('/users/aaaa')
+        .then(response => {
+            expect(response.status).toBe(500)   // expects error 500
+        }).catch(fail)  // global function to transform error in fail test
+})
+
+// testing PATCH
+test('patch /users/:id', () => {
+    return request(config.base_url)     // return to use a request like a promise
+        .post('/users')
+        .send({
+            name: 'vollare',
+            email: 'vollare@vollare.com',
+            password: '654321',
+            role: 'Certidão'
+        })
+        .then(response => request(config.base_url)
+                          .patch('/users/'+response.body._id)
+                          .send({
+                              name: 'lucas',
+                              role: 'Administrador'
+                          }))
+        .then(response => {
+            expect(response.status).toBe(200)   // awaits response status is 200
+            expect(response.body._id).toBeDefined()
+            expect(response.body.name).toBe('lucas')
+            expect(response.body.email).toBe('vollare@vollare.com')
+            expect(response.body.role).toBe('Administrador')
+            expect(response.body.password).toBeUndefined()   // because don't return password
+        })
+        .catch(fail)
 })
